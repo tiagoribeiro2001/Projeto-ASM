6d899ba1f9bc04f4d2c042bc9c985b6cb4880aad
Tiago Ribeiro <62017425+tiagoribeiro2001@users.noreply.github.com>
2023-04-18 23:59:20 +0100

    Inicio

---
+++ agents.py
@@ -0,0 +1,6 @@
+import random
+import time
+from spade.agent import Agent
+from spade.behaviour import CyclicBehaviour, PeriodicBehaviour
+from spade.message import Message
+from dados import XMPP_SERVER, PASSWORD
